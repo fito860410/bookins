89b4ece8dcf2407b7867791bd35cc6bf24a95bdc
Frangel <frangeldeveloper@gmail.com>
2023-05-11 06:38:08 -0600

    main

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,6 +24,11 @@ func routes(app *config.AppConfig) http.Handler{
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	//Declarando ubicacion en el servidor
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
 
